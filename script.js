59f15021bc8f2387a7eaf6437ce35144e9df73bd
praveenkumar-834 <praveenuu34@gmail.com>
2025-12-26 12:54:51 +0530

    final

---
+++ script.js
@@ -0,0 +1,21 @@
+
+// var element=document.createElement('div');
+// element.setAttribute('class','container');
+
+// element.innerHTML=("<div class=\"container\"><div class=\"row\"><div class=\"col\">Hello</div></div></div>");
+// document.body.appendChild(element);
+
+function click()
+{
+    var c = document.getElementById("input").value
+
+
+if (c >= 18)
+    {
+        alert("your are in");
+    }
+    else
+    {
+        alert("you are out");
+    }
+}
